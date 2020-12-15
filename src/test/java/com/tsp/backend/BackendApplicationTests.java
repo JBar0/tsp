bdcbfae2f2566899d4e4dcd57787f659f2a5d225
JBar0 <baranowski.jakub97@gmail.com>
2020-12-15 22:20:42 +0100

    init

---
+++ src/test/java/com/tsp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
